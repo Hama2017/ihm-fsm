34878ca8315f6ff3f6da908afa656df734a417b4
Hama2017 <hamaba2016@gmail.com>
2025-04-15 14:57:08 +0200

    f

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
-   <router-view />
-
+  <router-view />
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import TheSidebar from './components/layout/TheSidebar.vue';
-import TheHeader from './components/layout/TheHeader.vue';
-import TheFooter from './components/layout/TheFooter.vue';
-import AutomateList from './components/fsm/AutomateList.vue';
-import FunctionList from './components/fsm/FunctionList.vue';
-import StateList from './components/fsm/StateList.vue';
-import AutomateEditor from './components/fsm/AutomateEditor.vue';
-import UiTabs from './components/ui/UiTabs.vue';
-import UiForm from './components/ui/UiForm.vue';
-import UiButton from './components/ui/UiButton.vue';
-import UiModal from './components/ui/UiModal.vue';
-
-// State
-const isSidebarCollapsed = ref(false);
-const isModalOpen = ref(false);
+// Pas besoin d'imports ici puisque le routeur s'occupe de tout
 </script>
